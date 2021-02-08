4beb2c547447eebd4edc5607eb690a0b31fd9fbb
Victoria-IT <58858249+Victoria-IT@users.noreply.github.com>
2021-02-08 19:14:44 +0300

    Исправление Адреса this

--- Address.h
+++ Address.h
@@ -16,12 +16,12 @@ struct Address {
     Address(){}
     Address(string country, string region, string city, string street, string house, int post_code)
     {
-        return country;
-        return region;
-        return city;
-        return street;
-        return house;
-        return post_code;
+        this->country = country;
+        this->region = region;
+        this->city = city;
+        this->street = street;
+        this->house = house;
+        this->post_code = post_code;
     }
 };
 
